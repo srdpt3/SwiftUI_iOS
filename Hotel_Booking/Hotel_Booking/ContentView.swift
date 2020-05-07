64ef065d65e23ba66a8252fd3c8c1249fcfcc160
Dustin Yang <srdpt3@gmail.com>
2020-05-07 09:52:44 -0400

    update

---
+++ Hotel_Booking/Hotel_Booking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hotel_Booking
+//
+//  Created by Dustin yang on 5/7/20.
+//  Copyright © 2020 Dustin yang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
